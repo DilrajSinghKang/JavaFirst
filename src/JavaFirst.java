0a7b9ab090bdc1123c2fe2df5a79c830d98e59f6
Dilraj Kang <nsw.kang@gmail.com>
2022-06-02 09:03:22 +1000

    push JavaFirst

--- src/JavaFirst.java
+++ src/JavaFirst.java
@@ -1,4 +1,5 @@
 public class JavaFirst {
     public static void main(String[] args)
-    {System.out.println("Hello world Dilraj Kang");}
+    {System.out.println("Hello world Dilraj Kang");
+        System.out.println("Hello world This is Test");}
 }
